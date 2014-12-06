76a5dcb6fe298370b806771e57b209096b6ad235
Joseph D. Purcell <josephdpurcell@gmail.com>
2014-12-06 16:12:16 -0600

    Move methods to Perceptron struct.

--- perceptron.go
+++ perceptron.go
@@ -36,6 +36,60 @@ type Perceptron struct {
 }
 
 /**
+ * No idea what this method is doing.
+ */
+func (p Perceptron) train (input [3]float64, desired float64) {
+    var guess float64 = p.feedforward(input)
+    var error float64 = desired - guess
+    for i := 0; i < len(p.weights); i++ {
+        p.weights[i] += error * input[i] * p.learning
+    }
+    fmt.Printf("Our guess was: %f, error was: %f", guess, error)
+    fmt.Println()
+    if (guess >= 0) {
+        fmt.Printf("Our guess was that the point %f, %f is above the line f(x) = 10.", input[0], input[1])
+    } else {
+        fmt.Printf("Our guess was that the point %f, %f is below the line f(x) = 10.", input[0], input[1])
+    }
+    fmt.Printf("The weights are now %f, %f, %f.", p.weights[0], p.weights[0], p.weights[0])
+    fmt.Println()
+}
+
+/**
+ * Feedforward means: here are the inputs for the Perceptron, get the
+ * Perceptron to tell us the value.
+ *
+ * This function doesn't make sense to me yet.
+ */
+func (p Perceptron) feedforward (input [3]float64) float64 {
+    var sum float64 = 0
+
+    for i := 0; i < len(input); i++ {
+        sum += input[i] * p.weights[i]
+    }
+
+    return p.activate(sum)
+}
+
+/**
+ * This doesn't make sense to me yet.
+ */
+func (p Perceptron) activate (sum float64) float64 {
+    if (sum > 0) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * Get the weights.
+ */
+func (p Perceptron) getWeights () [3]float64 {
+    return p.weights
+}
+
+/**
  * Create a Perceptron.
  *
  * For now, I'm using 3 inputs: x, y, and bias. When I figure out how to have
@@ -55,56 +109,11 @@ func PerceptronFactory (n int, learning float64) Perceptron {
 }
 
 /**
- * Feedforward means: here are the inputs for the Perceptron, get the
- * Perceptron to tell us the value.
- *
- * This function doesn't make sense to me yet.
- */
-func feedforward (p Perceptron, input [3]float64) float64 {
-    var sum float64 = 0
-
-    for i := 0; i < len(input); i++ {
-        sum += input[i] * p.weights[i]
-    }
-
-    return activate(sum)
-}
-
-/**
- * This doesn't make sense to me yet.
- */
-func activate (sum float64) float64 {
-    if (sum > 0) {
-        return 1;
-    } else {
-        return -1;
-    }
-}
-
-/**
  * This is our line's definition.
  */
 func f(x float64) float64 {
     return 10
     //return 2*x + 1
-}
-
-/**
- * No idea what this method is doing.
- */
-func train (p Perceptron, input [3]float64, desired float64) {
-    var guess float64 = feedforward(p, input)
-    var error float64 = desired - guess
-    for i := 0; i < len(p.weights); i++ {
-        p.weights[i] += p.learning * error * input[i];
-    }
-    fmt.Println(guess)
-    if (guess >= 0) {
-        fmt.Printf("Our guess was that the point %f, %f is above the line f(x) = 10.", input[0], input[1])
-    } else {
-        fmt.Printf("Our guess was that the point %f, %f is below the line f(x) = 10.", input[0], input[1])
-    }
-    fmt.Println()
 }
 
 /**
@@ -119,34 +128,35 @@ type Trainer struct {
 /**
  * Create a Trainer based on a point.
  */
-func TrainerFactory (x float64, y float64) Trainer {
+func TrainerFactory () Trainer {
+    var xmin float64 = -400
+    var xmax float64 = 400
+    var ymin float64 = -100
+    var ymax float64 = 100
     var answer float64
+    var x float64 = random(xmin, xmax)
+    var y float64 = random(ymin, ymax)
+
     if (y < f(x)) {
         answer = -1
     } else {
         answer = 1
     }
+
     var input = [3]float64{x, y, 1}
     t := Trainer{
         input: input,
         answer: answer,
     }
+
     return t
 }
 
 func main() {
     // Setup the trainers.
-    var xmin float64 = -400
-    var xmax float64 = 400
-    var ymin float64 = -100
-    var ymax float64 = 100
-    var x float64
-    var y float64
-    var trainers [10]Trainer
+    var trainers [20000]Trainer
     for i := 0; i < len(trainers); i++ {
-        x = random(xmin, xmax)
-        y = random(ymin, ymax)
-        trainers[i] = TrainerFactory(x, y)
+        trainers[i] = TrainerFactory()
     }
 
     // Learning Constant is low just b/c it's fun to watch, this is not necessarily optimal
@@ -154,7 +164,7 @@ func main() {
 
     // Train our Perceptron.
     for i := 0; i < len(trainers); i++ {
-        train(p, trainers[i].input, trainers[i].answer)
+        p.train(trainers[i].input, trainers[i].answer)
     }
 }
 
